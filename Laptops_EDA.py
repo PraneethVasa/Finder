e45ec609e3eb152d89756d31d624e4d3df75c580
Vasa Purna Praneeth <128016507+PraneethVasa@users.noreply.github.com>
2023-07-20 22:35:53 +0530

    Update Laptops_EDA.py

--- Laptops_EDA.py
+++ Laptops_EDA.py
@@ -8,7 +8,7 @@ from PIL import Image
 
 #Setting Page Configuration
 slt.set_page_config(page_title = "Laptops - EDA",page_icon="icon.png",layout="wide")
-slt.title("@PraneethVasa")
+slt.caption("@PraneethVasa")
 slt.sidebar.title("Project Description")
 slt.sidebar.write("The Laptop Finder is a Python-based web app built with Streamlit that helps users discover the best laptops based on their specific requirements and preferences. Whether you're a student, a gamer, or a professional seeking a reliable workhorse, the Laptop Finder simplifies the process of finding the ideal laptop that meets your needs and budget.")
 # Creator Information
@@ -36,7 +36,7 @@ else:
         data = pd.read_csv("Laptop1.csv")
         slt.title("LAPTOP FINDER")
         slt.write("Confused about which laptop to buy? Just feed in your requirements to our Laptop Finder and you will get best recommendations according to your specifications")
-        if slt.button("Get Started",key = '100'):
+        if True:
              brand = slt.selectbox("Select Preferred Brand",['Lenovo','HP','DELL','APPLE','RedmiBook','SAMSUNG','MSI','realme Book','ASUS','acer','Infinix'])
              x1 = data[data['name'].str.contains(brand)]
              p = x1.processor.unique()
